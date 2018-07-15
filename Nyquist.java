f30d58c3242ba02c59133ec5e7f856acc0ea9255
PeTrA-Korea <gmlrnjs1994@gmail.com>
2018-07-15 23:17:06 +0900

    Nyquist Function is Uploaded :)

---
+++ Nyquist.java
@@ -0,0 +1,50 @@
+/**
+ * Project Computer Networking Introduce
+ * copyright PeTrA (c) 2018 All rights reserved.
+ * Republic Of Korea, Seoul :: https://github.com/purmirl
+ */
+
+package Function;
+
+/**
+ * Nyquist Class
+ * @author PeTrA
+ * 
+ * For Harry Nyquist(1889-1976)
+ */
+public class Nyquist {
+	public Nyquist() {}
+	
+	/**
+	 * Nyquist Channel Capacity Logic.
+	 * " C = 2B * log2 (M)"
+	 * C : Channel Capacity
+	 * B = Bandwidth
+	 * M = signalCountNumber
+	 * 
+	 * The Nyquist Channel Capacity Logic (NCL) don't care about any noise.
+	 * The formula for considering noise is Shannon's logic.
+	 * ------
+	 * @param bandwidth
+	 * @param signalCountNumber
+	 * @return channelCpacity
+	 */
+	public static long nyquistChannelCapacity(long bandwidth, long signalCountNumber) {
+		long channelCapacity = -1;
+		channelCapacity = 2 * bandwidth * (log2(signalCountNumber));
+		
+		return channelCapacity;
+	}
+	
+	/**
+	 * log2 function.
+	 * @param number
+	 * @return
+	 */
+	public static long log2(long number) {
+		long result = -1;
+		result = (long) (Math.log(number) / Math.log(2));
+		
+		return result;
+	}
+}
