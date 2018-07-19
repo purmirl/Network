0a8ff0282429d0fb51f27758aadc73a4213a9bf0
PeTrA-Korea <gmlrnjs1994@gmail.com>
2018-07-19 21:54:34 +0900

    Manchester class making :)

--- Manchester.java
+++ Manchester.java
@@ -9,18 +9,44 @@ package Function;
 /**
  * Manchester Class
  * @author PeTrA
+ * 
+ * The Manchester code, developed in the University of Manchester.
  */
 public class Manchester {
-	private int integerResultData;
-	private String stringResultData;
+	private int previousData;
+	private int currentData;
+	private String upwardTransition;
+	private String downwardTransition;
+	private String errorTransition;
 	
 	public Manchester() {
 		init();
 	}
 	
 	private void init() {
-		this.integerResultData = -1;
-		this.stringResultData = "";
+		this.previousData = -1;
+		this.currentData = -1;
+		this.upwardTransition = "UP";
+		this.downwardTransition = "DOWN";
+		this.errorTransition = "ERROR";
 	}
 	
+	/**
+	 * Manchester Encoding Logic.
+	 * Digital data -> Digital signal.
+	 * If digital data is 0 : digital signal is downward transition. ก้
+	 * If digital data is 1 : digital signal is upward transition. ก่
+	 * ------
+	 * @param digitalData
+	 * @return
+	 */
+	public String manchesterEncoding(int digitalData) {
+		if(digitalData == 0) {
+			return this.downwardTransition;
+		}else if(digitalData == 1) {
+			return this.upwardTransition;
+		}else {
+			return this.errorTransition;
+		}	
+	}
 }
