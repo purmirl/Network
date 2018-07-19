a1c9fa7cad7ea55105b27b1f640fe16fcc4d5abb
PeTrA-Korea <gmlrnjs1994@gmail.com>
2018-07-19 21:25:36 +0900

    Manchester class making :)

---
+++ Manchester.java
@@ -0,0 +1,26 @@
+/**
+ * Project Computer Networking Introduce
+ * copyright PeTrA (c) 2018 All rights reserved.
+ * Republic Of Korea, Seoul :: https://github.com/purmirl
+ */
+
+package Function;
+
+/**
+ * Manchester Class
+ * @author PeTrA
+ */
+public class Manchester {
+	private int integerResultData;
+	private String stringResultData;
+	
+	public Manchester() {
+		init();
+	}
+	
+	private void init() {
+		this.integerResultData = -1;
+		this.stringResultData = "";
+	}
+	
+}
