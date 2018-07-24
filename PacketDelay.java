c2448ebbfff27f0c89e7808cbc5c77efb99aaa29
PeTrA-Korea <gmlrnjs1994@gmail.com>
2018-07-24 16:05:26 +0900

    PacketDelay class is added :)

---
+++ PacketDelay.java
@@ -0,0 +1,32 @@
+/**
+ * Project Computer Networking Introduce
+ * copyright PeTrA (c) 2018 All rights reserved.
+ * Republic Of Korea, Seoul :: https://github.com/purmirl
+ */
+
+package Function;
+
+/**
+ * Packet transfer delay logic.
+ * ------
+ * Packet delay type:
+ *  1) Nodal processing delay
+ *  2) Queuing delay
+ *  3) Transmission delay
+ *  4) Propagation delay
+ *  
+ * Total nodal delay = Nodal processing delay + Queuing delay + Transmission delay + Propagation delay
+ * 
+ * 1) Nodal processing delay
+ * 	Calculate time of the packet trans where, in the route port.
+ * 
+ * 2) Queing delay
+ *  
+ * @author PeTrA
+ *
+ */
+public class PacketDelay {
+	public PacketDelay() {}
+	
+	
+}
