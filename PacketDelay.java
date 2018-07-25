39497cf141c6705c8d6f45b8931aa93c15589aa4
PeTrA-Korea <gmlrnjs1994@gmail.com>
2018-07-25 17:12:10 +0900

    PacketDelay class is modified :)

--- PacketDelay.java
+++ PacketDelay.java
@@ -8,7 +8,8 @@ package Function;
 
 /**
  * Packet transfer delay logic.
- * ------
+ * @author PeTrA
+ *
  * Packet delay type:
  *  1) Nodal processing delay
  *  2) Queuing delay
@@ -20,13 +21,57 @@ package Function;
  * 1) Nodal processing delay
  * 	Calculate time of the packet trans where, in the route port.
  * 
- * 2) Queing delay
+ * 2) Queuing delay
+ *  Waiting time of the packet in the queue stack.
  *  
- * @author PeTrA
- *
+ * 3) Transmission delay
+ * 	The time it takes to push it to the link.
+ * 
+ * 4) Propagation delay
+ *  Moving time in the link.
  */
 public class PacketDelay {
 	public PacketDelay() {}
 	
+	public double nodalProcessingDelay() {
+		double result = -1;
+		/**
+		 * Very small figures (msec)
+		 * Sending the packet to the queue after processing the node.
+		 */
+		return result;
+	}
 	
+	public double queuingDelay() {
+		double result = -1;
+		/**
+		 * msec, or Mu sec.
+		 * Very different from situation to situation.
+		 */
+		return result;
+	}
+	
+	/**
+	 * Transmission delay function.
+	 * @param _packetLength
+	 * @param _bitRate
+	 * @return
+	 */
+	public double transmissionDelay(double _packetLength, double _bitRate) {
+		double result = -1;
+		/**
+		 * Mu sec ~ msec
+		 */
+		result = _packetLength / _bitRate;
+		return result;
+	}
+	
+	public double propagationDelay() {
+		double result = -1;
+		/**
+		 * It depends on the physical medium.
+		 * 2 * 100000000 m/s ~ 3 * 100000000 m/s
+		 */
+		return result;
+	}
 }
