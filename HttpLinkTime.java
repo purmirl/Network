28f08b2023e49ee98a61a098e04f567c31a73742
PeTrA-Korea <gmlrnjs1994@gmail.com>
2018-07-26 23:53:26 +0900

    HttpLinkTime class is added :)

---
+++ HttpLinkTime.java
@@ -0,0 +1,17 @@
+/**
+ * Project Computer Networking Introduce
+ * copyright PeTrA (c) 2018 All rights reserved.
+ * Republic Of Korea, Seoul :: https://github.com/purmirl
+ */
+
+package Function;
+
+/**
+ * Http Link Time class.
+ * @author PeTrA
+ *
+ */
+public class HttpLinkTime {
+	public HttpLinkTime() {}
+	
+}
