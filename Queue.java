1baef94f95c364ba3b3b27b471cbb1175dedb511
PeTrA-Korea <gmlrnjs1994@gmail.com>
2018-07-27 21:35:23 +0900

    Queue class is added :)

---
+++ Queue.java
@@ -0,0 +1,30 @@
+/**
+ * Project Computer Networking Introduce
+ * copyright PeTrA (c) 2018 All rights reserved.
+ * Republic Of Korea, Seoul :: https://github.com/purmirl
+ */
+
+package Function;
+
+/**
+ * Queue Class
+ * @author PeTrA
+ *
+ * Queue : Abstract data type
+ * First in first out data structure. (FIFO)
+ * In Switch, Router, It is used as a place where packets wait.
+ * (Queue delay)
+ * ------
+ * Time complexity in big O notation.
+ * Algorithm	Average	Worst case
+ * Space		O(n)	O(n)
+ * Search		O(n)	O(n)
+ * Insert		O(1)	O(1)
+ * Delete		O(1)	O(1)
+ *
+ * Reference : https://en.wikipedia.org/wiki/Queue_(abstract_data_type)
+ */
+public class Queue {
+	public Queue() {}
+	
+}
