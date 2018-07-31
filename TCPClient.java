53a9ddd92b7a9e0e7a66a48f0b76f2654b80397a
PeTrA-Korea <gmlrnjs1994@gmail.com>
2018-07-31 18:36:17 +0900

    TCPClient class is added :)

---
+++ TCPClient.java
@@ -0,0 +1,13 @@
+/**
+ * Project Computer Networking Introduce
+ * copyright PeTrA (c) 2018 All rights reserved.
+ * Republic Of Korea, Seoul :: https://github.com/purmirl
+ */
+
+package Function;
+
+public class TCPClient {
+	public TCPClient() {}
+	
+	
+}
