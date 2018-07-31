2abb99b9e6b3c5941f299c51b7f3ea9b56c8c11e
PeTrA-Korea <gmlrnjs1994@gmail.com>
2018-07-31 20:05:20 +0900

    Making the TCPClient class :)

--- TCPClient.java
+++ TCPClient.java
@@ -7,7 +7,19 @@
 package Function;
 
 public class TCPClient {
-	public TCPClient() {}
+	private String ip;
+	private int port;
 	
+	public TCPClient(String _ip, int _port) {
+		this.ip = _ip;
+		this.port = _port;
+	}
 	
+	public void setConnectionIpAddress(String _ip) {
+		this.ip = _ip;
+	}
+	
+	public void setConnectionPortNumber(int _port) {
+		this.port = _port;
+	}
 }
