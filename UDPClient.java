7a9d5925c7a0881a7ccd5e6297a1c0409c88978e
PeTrA-Korea <gmlrnjs1994@gmail.com>
2018-08-02 02:53:00 +0900

    Making the UDPClient class :)

--- UDPClient.java
+++ UDPClient.java
@@ -6,7 +6,35 @@
 
 package Function;
 
+import java.net.DatagramSocket;
+
+/**
+ * UDP Client class.
+ * @author PeTrA
+ *
+ * UDP Client Pseudo code.
+ * Non persistant connection.
+ */
 public class UDPClient {
-	public UDPClient() {}
+	private String ip;
+	private int port;
+	private DatagramSocket socket;
+
+	public UDPClient(String _ip, int _port, DatagramSocket _socket) {
+		this.ip = _ip;
+		this.port = _port;
+		this.socket = _socket;
+	}
 	
+	public void setServerIpAddress(String _ip) {
+		this.ip = _ip;
+	}
+	
+	public void setServerPortNumber(int _port) {
+		this.port = _port;
+	}
+	
+	public void setSocket(DatagramSocket _socket) {
+		this.socket = _socket;
+	}
 }
