3ac24c2dc9567e1e338554dcb962ba56971c02ce
PeTrA-Korea <gmlrnjs1994@gmail.com>
2018-08-08 19:48:21 +0900

    UDP Client class is modified :)

--- UDPClient.java
+++ UDPClient.java
@@ -6,7 +6,11 @@
 
 package Function;
 
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
 /**
  * UDP Client class.
@@ -14,16 +18,16 @@ import java.net.DatagramSocket;
  *
  * UDP Client Pseudo code.
  * Non persistant connection.
+ * 
+ * Reference : https://en.wikipedia.org/wiki/User_Datagram_Protocol
  */
 public class UDPClient {
 	private String ip;
 	private int port;
-	private DatagramSocket socket;
 
-	public UDPClient(String _ip, int _port, DatagramSocket _socket) {
+	public UDPClient(String _ip, int _port) {
 		this.ip = _ip;
 		this.port = _port;
-		this.socket = _socket;
 	}
 	
 	public void setServerIpAddress(String _ip) {
@@ -34,7 +38,14 @@ public class UDPClient {
 		this.port = _port;
 	}
 	
-	public void setSocket(DatagramSocket _socket) {
-		this.socket = _socket;
+	public void receive() {
+		
+	}
+	
+	public void send(String _data) throws SocketException, UnknownHostException {
+		DatagramSocket socket = new DatagramSocket();
+		InetAddress address = InetAddress.getByName(this.ip);
+		DatagramPacket sendData = new DatagramPacket(_data.getBytes(), _data.getBytes().length, address, this.port);
+		
 	}
 }
