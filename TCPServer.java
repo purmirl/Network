06073f83b51ae85270fac5f7d2af5064d69dbf17
PeTrA-Korea <gmlrnjs1994@gmail.com>
2018-08-16 23:44:39 +0900

    TCPServer class is added :)

---
+++ TCPServer.java
@@ -0,0 +1,18 @@
+/**
+ * Project Computer Networking Introduce
+ * copyright PeTrA (c) 2018 All rights reserved.
+ * Republic Of Korea, Seoul :: https://github.com/purmirl
+ */
+
+package Function;
+
+/**
+ * TCPServer Class
+ * @author PeTrA
+ *
+ *
+ */
+public class TCPServer {
+	public TCPServer() {}
+	
+}
