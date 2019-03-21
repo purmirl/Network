36b4d48e7ed7aa6b9d4bdb8822ac3b3181a0415d
PeTrA-Korea <gmlrnjs1994@gmail.com>
2019-03-21 19:08:05 +0900

    Add the Udp.java

---
+++ Udp.java
@@ -0,0 +1,34 @@
+/**
+ * Project Computer Networking Introduce
+ * copyright PeTrA (c) 2019 All rights reserved.
+ * Republic Of Korea, Seoul :: https://github.com/purmirl
+ */
+package Protocol;
+
+/**
+ * UDP Class
+ * @author PeTrA
+ *
+ * UDP : User Datagram Protocol
+ * 
+ * Transport layer protocol
+ * 
+ * Standard : RFC 768, 1122
+ * Protocol ID : 17
+ * ------
+ * Reference
+ *  https://en.wikipedia.org/wiki/User_Datagram_Protocol
+ *  http://www.ktword.co.kr/abbr_view.php?m_temp1=323
+ */
+public class Udp {
+	/**
+	 * Header value.
+	 */
+	private byte[] sourceIpAddress;
+	private byte[] destinationIpAddress;
+	private byte[] sourcePort;
+	private byte[] destinationPort;
+	private byte[] length;
+	
+	
+}
